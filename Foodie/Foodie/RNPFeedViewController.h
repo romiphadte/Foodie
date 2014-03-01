e495a75625a644d81d6db027b815402ce2fb9a39
Neeraj Baid <wdaareg@gmail.com>
2014-02-28 21:36:44 -0800

    Added base Feed files.

---
+++ Foodie/Foodie/RNPFeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  RNPFeedViewController.h
+//  Foodie
+//
+//  Created by Neeraj Baid on 2/28/14.
+//  Copyright (c) 2014 Romi Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNPFeedViewController : UIViewController
+
+@end
---
+++ Foodie/Foodie/RNPFeedViewController.m
@@ -0,0 +1,38 @@
+//
+//  RNPFeedViewController.m
+//  Foodie
+//
+//  Created by Neeraj Baid on 2/28/14.
+//  Copyright (c) 2014 Romi Phadte. All rights reserved.
+//
+
+#import "RNPFeedViewController.h"
+
+@interface RNPFeedViewController ()
+
+@end
+
+@implementation RNPFeedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
