061b0fb1a54b60991e49516ce56c2df5078e1c51
Romi Phadte <romiphadte@gmail.com>
2014-03-11 15:57:21 -0700

    added basic files

---
+++ Foodie/Foodie/RNPLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  RNPLoginViewController.h
+//  Foodie
+//
+//  Created by Romi Phadte on 3/11/14.
+//  Copyright (c) 2014 Romi Phadte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNPLoginViewController : UIViewController
+
+@end
---
+++ Foodie/Foodie/RNPLoginViewController.m
@@ -0,0 +1,40 @@
+//
+//  RNPLoginViewController.m
+//  Foodie
+//
+//  Created by Romi Phadte on 3/11/14.
+//  Copyright (c) 2014 Romi Phadte. All rights reserved.
+//
+
+#import "RNPLoginViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
+
+
+@interface RNPLoginViewController ()
+
+@end
+
+@implementation RNPLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
