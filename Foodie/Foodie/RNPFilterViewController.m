c81c092ab78f502c09178f1742062b6b882b6c2f
Romi Phadte <romiphadte@gmail.com>
2014-03-27 11:34:40 -0700

    adding filter image stuff

--- Foodie/Foodie/RNPFilterViewController.m
+++ Foodie/Foodie/RNPFilterViewController.m
@@ -13,10 +13,12 @@
 @interface RNPFilterViewController ()
 
 @property UIImage *savedImage;
+@property UIImage *filteredImage;
 @property (weak, nonatomic) IBOutlet FXBlurView *topBlurBar;
 @property (weak, nonatomic) IBOutlet UIView *bottomBlurBar;
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 
 @end
 
@@ -73,5 +75,18 @@
     }];
 
 }
+
+- (IBAction)sepia:(id)sender {
+    GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:inputImage];
+    GPUImageSepiaFilter *stillImageFilter = [[GPUImageSepiaFilter alloc] init];
+    
+    [stillImageSource addTarget:stillImageFilter];
+    [stillImageFilter useNextFrameForImageCapture]
+    [stillImageSource processImage];
+    
+    UIImage *currentFilteredVideoFrame = [stillImageFilter imageFromCurrentFramebuffer];
+}
+
+
 
 @end
